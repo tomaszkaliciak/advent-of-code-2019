9a6a6511bbf0e7c1f590234f13b1126bf40a26bb
Tomasz Kaliciak <226543@student.pwr.edu.pl>
2020-01-06 21:26:35 +0100

    day one

---
+++ day1/solution.py
@@ -0,0 +1,9 @@
+
+weightsOfModules = open("input.txt", "r")
+
+fuelRequired = 0
+
+for weightOfModule in weightsOfModules:
+    fuelRequired += max(0,(int(weightOfModule) // 3 ) - 2)
+
+print(f"We need: {fuelRequired} fuel")
