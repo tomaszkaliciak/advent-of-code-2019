86d1058091b2a0d8e29747348c924c33572c6cd3
Tomasz Kaliciak <226543@student.pwr.edu.pl>
2020-02-05 00:07:45 +0100

    small improvement

--- day2/part1.cpp
+++ day2/part1.cpp
@@ -19,23 +19,19 @@ int main()
     codes[1] = 12;
     codes[2] = 2;
 
-    for (auto i = 0; i < codes.size() - 4; i += 4)
+    for (auto i = 0; i < codes[i] != 99; i += 4)
     {
         const auto operationType = codes[i];
         if (operationType == 1)
         {
             codes[codes[i + 3]] = codes[codes[i + 1]] + codes[codes[i + 2]];
         }
-        else if (operationType == 2)
+        else
         {
             codes[codes[i + 3]] = codes[codes[i + 1]] * codes[codes[i + 2]];
         }
-        else
-        {
-            break;
-        }
     }
-
+    
     std::cout << codes[0] << "\n";
 
     return 0;
