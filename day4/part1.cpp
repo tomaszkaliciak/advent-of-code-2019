4bf7e6b4883416c4f48282aecf841557a5083fdc
Tomasz Kaliciak <226543@student.pwr.edu.pl>
2020-02-09 22:58:13 +0100

    better solution

--- day4/part1.cpp
+++ day4/part1.cpp
@@ -2,15 +2,13 @@
 #include <string>
 #include <algorithm>
 
-bool isSixDigitsLong(const uint number)
+bool isSixDigitsLong(const std::string& digits)
 {
-    return std::to_string(number).length() == 6;
+    return digits.length() == 6;
 }
 
-bool doesDigitsNeverDecrease(const uint number)
+bool doesDigitsNeverDecrease(const std::string& digits)
 {
-    std::string digits(std::to_string(number));
-
     return digits[0] <= digits[1]
         and digits[1] <= digits[2]
         and digits[2] <= digits[3]
@@ -18,10 +16,8 @@ bool doesDigitsNeverDecrease(const uint number)
         and digits[4] <= digits[5];
 }
 
-bool doesAnyOfAdjacentDigitsAreTheSame(const uint number)
+bool doesAnyOfAdjacentDigitsAreTheSame(const std::string& digits)
 {
-    std::string digits(std::to_string(number));
-
     return std::adjacent_find(digits.begin(), digits.end()) != digits.end();
 }
 
@@ -33,9 +29,10 @@ int main()
 
     for (auto number = lowerBound; number <= upperBound; ++number)
     {
-        if (isSixDigitsLong(number) and
-            doesDigitsNeverDecrease(number) and
-            doesAnyOfAdjacentDigitsAreTheSame(number))
+        if (std::string digits(std::to_string(number));
+            isSixDigitsLong(digits) and
+            doesDigitsNeverDecrease(digits) and
+            doesAnyOfAdjacentDigitsAreTheSame(digits))
         {
             ++numbersThatMeetCriteria;
         }
